4256be5d1d53cebb2f8ad7adbe7b7eb572f50a84
Mateusz Mesek <mateusz@mesek.pl>
2022-03-16 04:32:42 +0100

    wip

---
+++ src/DataResolver.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace MateuszMesek\DocumentDataCmsPageIndexer;
+
+use MateuszMesek\DocumentDataCmsPage\Command\GetDocumentDataByPageIdAndStoreId;
+use MateuszMesek\DocumentDataIndexerApi\DataResolverInterface;
+use MateuszMesek\DocumentDataIndexerApi\DimensionResolverInterface;
+use Traversable;
+
+class DataResolver implements DataResolverInterface
+{
+    private DimensionResolverInterface $storeIdResolver;
+    private GetDocumentDataByPageIdAndStoreId $getDocumentDataByPageId;
+
+    public function __construct(
+        DimensionResolverInterface $storeIdResolver,
+        GetDocumentDataByPageIdAndStoreId $getDocumentDataByPageId
+    )
+    {
+        $this->storeIdResolver = $storeIdResolver;
+        $this->getDocumentDataByPageId = $getDocumentDataByPageId;
+    }
+
+    public function resolve(array $dimensions, Traversable $entityIds): Traversable
+    {
+        $storeId = $this->storeIdResolver->resolve($dimensions);
+
+        foreach ($entityIds as $entityId) {
+            $data = $this->getDocumentDataByPageId->execute($entityId, $storeId);
+
+            if (empty($data)) {
+                return;
+            }
+
+            yield $data;
+        }
+    }
+}
---
+++ src/EntityIdsResolver.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+
+namespace MateuszMesek\DocumentDataCmsPageIndexer;
+
+use Magento\Cms\Api\Data\PageInterface;
+use Magento\Cms\Model\ResourceModel\Page\CollectionFactory;
+use Magento\Framework\DB\Select;
+use MateuszMesek\DocumentDataIndexerApi\DimensionResolverInterface;
+use MateuszMesek\DocumentDataIndexerApi\EntityIdsResolverInterface;
+use Traversable;
+
+class EntityIdsResolver implements EntityIdsResolverInterface
+{
+    private DimensionResolverInterface $storeIdResolver;
+    private CollectionFactory $collectionFactory;
+
+    public function __construct(
+        DimensionResolverInterface $storeIdResolver,
+        CollectionFactory $collectionFactory
+    )
+    {
+        $this->storeIdResolver = $storeIdResolver;
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    public function resolve(array $dimensions): Traversable
+    {
+        $storeId = $this->storeIdResolver->resolve($dimensions);
+
+        $collection = $this->collectionFactory->create();
+        $collection->addStoreFilter($storeId);
+        $collection->setOrder(PageInterface::PAGE_ID, $collection::SORT_ORDER_ASC);
+        $collection->setPageSize(100);
+        $collection->getSelect()
+            ->reset(Select::COLUMNS)
+            ->columns([PageInterface::PAGE_ID])
+            ->setPart('disable_staging_preview', true);
+
+        $lastId = 0;
+
+        while (true) {
+            $part = (clone $collection);
+            $part->getSelect()->where('page_id > ?', $lastId);
+            $part->load();
+
+            $ids = array_map(
+                static function (PageInterface $page) {
+                    return (int)$page->getId();
+                },
+                $part->getItems()
+            );
+
+            if (empty($ids)) {
+                return;
+            }
+
+            $lastId = end($ids);
+
+            yield from $ids;
+        }
+    }
+}
